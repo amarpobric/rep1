84890b7f56631d9b6404cdab105473ddb685ad4f
Amar <amar.pobric@edu.fit.ba>
2020-11-17 13:17:18 +0100

    secondCommit

---
+++ WebAppEnterwell/Controllers/PDVController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppEnterwell.Models;
+
+namespace WebAppEnterwell.Controllers
+{
+    public class PDVController : Controller
+    {
+        public ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Create()
+        {
+            var model = new PDVCreateViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Create(PDVCreateViewModel model)
+        {
+            var newPDV = new PDV()
+            {
+                Name=model.Name,
+                Value=model.Value
+            };
+            db.PDV.Add(newPDV);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebAppEnterwell/Models/PDVCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppEnterwell.Models
+{
+    public class PDVCreateViewModel
+    {
+        [Required(ErrorMessage = "Obavezan unos")]
+        [Display(Name = "Naziv")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Obavezan unos")]
+        [Display(Name = "Vrijednost pdva u %")]
+        public int Value { get; set; }
+    }
+}
